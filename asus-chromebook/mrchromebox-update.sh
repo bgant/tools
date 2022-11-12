bb3287aeaf8b0fd07538f484530569ea0f584bcb
Brandon Gant <bgant@protonmail.com>
2022-11-12 09:47:23 -0600

    grub fix

--- asus-chromebook/mrchromebox-update.sh
+++ asus-chromebook/mrchromebox-update.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/zsh
 # Source: https://mrchromebox.tech/#fwscript
+# sudo vi /etc/default/grub
+#    Add iomem=relaxed to GRUB_CMDLINE_LINUX_DEFAULT
+# sudo update-grub
 
 curl -LO mrchromebox.tech/firmware-util.sh && sudo bash firmware-util.sh
