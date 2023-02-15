10670a61139b450113e0c3febc85f3cefeab9605
Brandon Gant <bgant@protonmail.com>
2023-02-15 10:28:25 -0600

    add randomness

--- alpine-zfs-docker/eclipse-mosquitto/light_control.py
+++ alpine-zfs-docker/eclipse-mosquitto/light_control.py
@@ -5,8 +5,8 @@
 # pip install wheel paho-mqtt requests
 # chmod +x light_control.py
 # crontab -e  # Turn ON lights at 4:50 and Turn OFF at 9:15 and 10:00PM (just in case)
-# 50     16       *       *       *       /root/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto/light_control.py
-# 15     21       *       *       *       /root/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto/light_control.py
+# 30     16       *       *       *       /root/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto/light_control.py
+# 01     21       *       *       *       /root/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto/light_control.py
 # 00     22       *       *       *       /root/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto/light_control.py
 
 
@@ -14,22 +14,30 @@ from datetime import datetime
 import solar_api
 from FrontOutsideLights import FrontOutsideLights
 from time import sleep
+from random import randint
 
-if datetime.now() > datetime.now().replace(hour = 21, minute = 0, second = 0): 
-    print("Turning front outside lights OFF")
-    FrontOutsideLights('OFF')     # Turn lights OFF after 9PM
-else:
-    dusk = solar_api.phase('dusk')
-    while datetime.now() < dusk:
-        print('waiting for dusk at', dusk.strftime('%H:%M:%S'))
-        sleep(600)  # Wait 10 Minutes 
+try:
+    if datetime.now() > datetime.now().replace(hour = 21, minute = 0, second = 0): 
+        sleep(randint(120,840))       # Wait 2 to 14 Minutes to avoid looking like a Timer
+        print("Turning front outside lights OFF")
+        FrontOutsideLights('OFF')     # Turn lights OFF after 9PM
     else:
-        print("Turning front outside lights ON")
-        FrontOutsideLights('ON')  # Turn lights ON after Dusk
+        solar_phase = solar_api.phase('sunset')
+        while datetime.now() < solar_phase:
+            print('waiting for sunset at', solar_phase.strftime('%H:%M:%S'))
+            sleep(randint(300,900))   # Wait 5 to 15 Minutes to avoid looking like a Timer
+        else:
+            print("Turning front outside lights ON")
+            FrontOutsideLights('ON')  # Turn lights ON after Sunset
+except KeyboardInterrupt:
+    pass
 
 
 '''
-Jun 21 2023 'dusk': '8:58:30 PM'
-Dec 21 2023 'dusk': '5:01:51 PM'
+Jun 21 2023 'sunset': '8:25:36 PM'
+Dec 21 2023 'sunset': '4:31:15 PM'
+
+Jun 21 2023   'dusk': '8:58:30 PM'
+Dec 21 2023   'dusk': '5:01:51 PM'
 '''
 
