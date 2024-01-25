9ce1901e696d4caded6a4c4f4fd8967273b3fe15
Brandon Gant <bgant@protonmail.com>
2024-01-24 20:49:44 -0600

    turn lights off to check for errors

--- alpine-zfs-docker/zigbee2mqtt/restart_zigbee2mqtt.sh
+++ alpine-zfs-docker/zigbee2mqtt/restart_zigbee2mqtt.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+#
+# apk add mosquitto_clients
+#
+# crontab -l
+# 00     16       *       *       *       /root/source/bgant/tools/alpine-zfs-docker/zigbee2mqtt/restart_zigbee2mqtt.sh
+# 10     16       *       *       *       /root/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto/light_control.py
+# 01     21       *       *       *       /root/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto/light_control.py
+# 00     22       *       *       *       /root/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto/light_control.py
 
-if [ `docker logs --since 25h zigbee2mqtt | grep -c Zigbee2MQTT:error` != 0 ]
+echo "Sending Request to Turn Lights Off..."
+mosquitto_pub -h 172.17.0.1 -p 1883 -t 'zigbee2mqtt/FrontOutsideLights/set' -m '{ "state": "OFF" }'
+echo "Waiting 30 seconds for any errors..."
+sleep 30
+if [ `docker logs --since 2m zigbee2mqtt | grep -c Zigbee2MQTT:error` != 0 ]
 then 
    echo "Errors Found in Logs... Restarting Container..."
    docker restart zigbee2mqtt
 else
    echo "No Errors in Logs... Exiting"
 fi
+
