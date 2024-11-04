904e3e8187a2e42d6ae5ed2ad53a9e6bf25891ff
Brandon Gant <bgant@protonmail.com>
2024-11-03 18:12:33 -0600

    handbrakecli

--- handbrake/handbrake.sh
+++ handbrake/handbrake.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/bash
 
-#echo -n "Enter Filename (without .mkv): "
-#read filename
-#INPUT=$filename.mkv
-#OUTPUT=$filename-output.mkv
-
 for input in *.mkv; do
 if [ -f "$input" ]; then
 
@@ -12,7 +7,6 @@ echo "############################################################"
 echo "$input"
 extension="${input##*.}"
 filename="${input%.*}"
-#output="${filename}-handbrake.${extension}"
 output="output/${input}"
 #taskset -c 1,3,5,7,9,11 \
 flatpak run fr.handbrake.HandBrakeCLI \
@@ -26,11 +20,11 @@ flatpak run fr.handbrake.HandBrakeCLI \
 --mixdown 5point1,stereo \
 --aname "Surround 5.1","Stereo","Commentary" \
 --subtitle-lang-list eng \
---all-subtitles
+--all-subtitles \
+--subtitle-default="none" \
+--subtitle-burned="none"
 
 fi
 done
 
-# --ab 448K
-# --mixdown 5point1
-# --arate auto
+# --start-at seconds:600 --stop-at seconds:120
