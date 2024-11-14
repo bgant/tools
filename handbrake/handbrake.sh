f34fef2916f17a01e49661711c30b4ec00355adc
Brandon Gant <bgant@protonmail.com>
2024-11-14 13:28:09 -0600

    using parallel

--- handbrake/handbrake.sh
+++ handbrake/handbrake.sh
@@ -20,29 +20,36 @@ fi
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")  # Spaces in filenames
+COMMAND_LIST=()
 for input in *.mkv; do
 
-echo "############################################################"
-echo "$input"
+#echo "############################################################"
+#echo "$input"
 extension="${input##*.}"
 filename="${input%.*}"
 output="output/${input}"
 #taskset -c 1,3,5,7,9,11 \
-$COMMAND \
---input $input \
---output $output \
---preset "Roku 2160p60 4K HEVC Surround" \
+COMMAND_LIST+=("$COMMAND \
+--input \"$input\" \
+--output \"$output\" \
+--preset \"Roku 2160p60 4K HEVC Surround\" \
 --crop-mode none \
 --audio-lang-list eng \
 --all-audio \
 --aencoder aac \
 --mixdown 5point1,stereo \
---aname "Surround 5.1","Stereo","Commentary" \
+--aname \"Surround 5.1\",\"Stereo\",\"Commentary\" \
 --native-language eng \
---subtitle "scan" \
---subtitle-forced
-
+--subtitle \"scan\" \
+--subtitle-forced")
+# --start-at seconds:600 --stop-at seconds:120
 done
+
+#echo ${COMMAND_LIST[@]}  # List all elements in array
+#echo ${COMMAND_LIST[20]} # List element 20 in array
+
+# apt install parallel
+nice parallel --keep-order --progress --eta --jobs 3 ::: "${COMMAND_LIST[@]}"
 IFS=$SAVEIFS
 
-# --start-at seconds:600 --stop-at seconds:120
+# View output of any running process with: tail -f /proc/<pid>/fd/1
