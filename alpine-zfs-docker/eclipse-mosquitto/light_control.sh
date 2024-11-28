bf46e54cb7cba2cd0636f50b2c3c9062bfb23670
Brandon Gant <bgant@protonmail.com>
2024-11-28 15:13:16 -0600

    moved alpine-zfs-docker files to docker repo

--- alpine-zfs-docker/eclipse-mosquitto/light_control.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker run --rm -v /home/docker/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto:/usr/src/myapp -w /usr/src/myapp python-pip python light_control.py
-
-# No docker run -it (interactive and terminal TTY) in crontab scripts!
