8b4fac0acc62cb69e2665c1ad58d0ec22eaef95b
Brandon Gant <bgant@protonmail.com>
2024-11-27 23:08:36 -0600

    no -it in crontab scripts

--- alpine-zfs-docker/eclipse-mosquitto/light_control.sh
+++ alpine-zfs-docker/eclipse-mosquitto/light_control.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker run -it --rm -v /home/docker/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto:/usr/src/myapp -w /usr/src/myapp python-pip python light_control.py
+docker run --rm -v /home/docker/source/bgant/tools/alpine-zfs-docker/eclipse-mosquitto:/usr/src/myapp -w /usr/src/myapp python-pip python light_control.py
+
+# No docker run -it (interactive and terminal TTY) in crontab scripts!
